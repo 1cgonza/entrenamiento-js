3d211255f202ebbd3130faf932758a216eff7555
3st3r30sc0p10 <h.idarraga@uniandes.edu.co>
2020-10-07 23:09:50 -0500

    Avances ejercicio 3

--- Ejercicios/03 - OOP clases/src/componentes/Ojos.js
+++ Ejercicios/03 - OOP clases/src/componentes/Ojos.js
@@ -1,23 +1,34 @@
-export default class Ojos {
-  constructor(ctx) {
-    this.ctx = ctx;
-    this.x = 0;
-    this.y = 0;
+export default class Ojos {//Se exporta la clase Ojos para que sea utilizada por index.js
+  constructor(ctx) {//se crea una función llamada constructor con una valor de ctx
+    this.ctx = ctx;//el valor de ctx es igual al valor ctx que pertenece al objeto al que se está refiriendo la función
+    this.x = 0;//el valor de x dentro del objeto al que hace referencia la función es 0
+    this.y = 0;//el valor de y dentro del objeto al que hace referencia la función es 0
   }
 
-  pintar(x, y) {
-    const margenX = 20;
-    const margenY = 10;
-    const ancho = 15;
-    const alto = 15;
+  pintar(x, y) {//Se crea la función pintar con los valores x, y, definidos en la función anterior
+    const margenX = 20;//Se crea una variable constante que determina el margen de x
+    const margenY = 10;//Se crea una variable constante que determina el margen de y
+    const ancho = 15;//Se crea una variable constante que determina el ancho del rectángulo
+    const alto = 15;//Se crea una variable constante que determina el alto del rectángulo
+    const radio = 15;
 
-    const ctx = this.ctx;
-    const ojoIzX = x - margenX - margenX / 2;
-    const ojoIzY = y - margenY;
-    const ojoDeX = x + margenX;
-    const ojoDeY = ojoIzY;
+    const ctx = this.ctx;//Se crea una variable constante en donde se declara que el valor de ctx es aquél que proviene del valor de la funcion constructor
+    const ojoIzX = x - margenX - margenX / 2;//Se crea una variable constante que define que el valor de x para ojo izquierdo, que es el resultado de 0 - margen x y de nuevo - margen x; todo ello dividido en 2
+    const ojoIzY = y - margenY;//Se crea una variable constante que define que el valor de y para ojo izquierdo, que es el resultado de 0 - margen y
+    const ojoDeX = x + margenX;//Se crea una variable constante que define que el valor de x para ojo derecho, que es el resultado de 0 + margen x
+    const ojoDeY = ojoIzY;//Se crea una variable constante que define que el valor de y en ojo derecho es igual al valor de y en ojo izquierdo
 
-    ctx.fillRect(ojoIzX, ojoIzY, ancho, alto);
-    ctx.fillRect(ojoDeX, ojoDeY, ancho, alto);
+
+    ctx.beginPath();//Crea una nueva ruta del objeto eliminando las sub-rutas anteriores
+    ctx.arc(ojoIzX, ojoIzY, radio, 0, 2 * Math.PI);//Determina el tamaño del arco del ojo izquierdo
+    ctx.arc(ojoDeX, ojoDeY, radio, 0, 2 * Math.PI);//Determina el tamaño del arco del ojo derecho
+    ctx.fill();
+    //ctx.fillRect(ojoIzX, ojoIzY, ancho, alto);//se determina el tamaño y posición del ojo izquierdo          
+    //ctx.fillRect(ojoDeX, ojoDeY, ancho, alto);//se determina el tamaño y posición del ojo derecho
+
+    ctx.fillStyle = 'green';//se determina el color de ambos ojos
+    ctx.lineWidth = 2;//se establece el ancho de la línea del contorno
+    ctx.strokeStyle = '#ffffff';//color de la línea del contorno
+    ctx.stroke();//Se determina que hay un contorno
   }
 }
--- Ejercicios/03 - OOP clases/src/index.js
+++ Ejercicios/03 - OOP clases/src/index.js
@@ -1,27 +1,27 @@
 import './scss/styles.scss';
-import Ojos from './componentes/Ojos';
+import Ojos from './componentes/Ojos';//Importa la clase ojos de Ojos.js
 
-const contenedor = document.getElementById('main');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const ojos = new Ojos(ctx);
-let ancho = 0;
-let alto = 0;
-let x = 0;
-let y = 0;
+const contenedor = document.getElementById('main');//Declara una variable constante que apunta al div main
+const canvas = document.getElementById('canvas');//Declara una variable constante que apunta al div canvas
+const ctx = canvas.getContext('2d');//declara la variable constante ctx. El método getContext, que sólo funciona junto al objeto canvas, permite crear un contexto dentro del cual hacer un dibujo. El argumento 2d se refiere a la creación de un objeto en 2 dimensiones gracias a la interfaz CanvasRenderingContext2D, que es la que brinda el contexto de renderizado
+const ojos = new Ojos(ctx);//Se crea una variable constante con la que se construye un objeto Ojos
+let ancho = 0;//Se crea una variable con el ancho de cada ojo
+let alto = 0;//Se crea una variable con el alto de cada ojo
+let x = 0;//se crea una variable con la posición x de cada ojo
+let y = 0;//Se crea una variable con la posición y de cada ojo
 
-actualizarDimensiones();
+actualizarDimensiones();//Se declara una función vacía
 
-contenedor.addEventListener('mousemove', (e) => {
-  const _x = e.clientX - x;
-  const _y = e.clientY - y;
+contenedor.addEventListener('mousemove', (e) => {//Dentro del div main se activa un evento cuando el mouse se mueve. la 'e' denota el evento y así podemos llamar el movimiento del mouse por medio de la 'e'
+  const _x = e.clientX - x;//Se declara una variable constante en la que el evento de mover el mouse determina el valor horizontal de _x
+  const _y = e.clientY - y;//Se declara una variable constante en la que el evento de mover el mouse determina el valor vertical de _y
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ojos.pintar(_x, _y);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);//ctx es la variable constante que permite dibujar objetos 2d dentro del canvas. El método clearRect impide que los lugares por donde pasen los ojos queden dibujados.
+  ojos.pintar(_x, _y);//Se crea un nuevo objeto a partir de la clase Ojos, que se importa del archivo Ojos.js, a partir de la función pintar. Los valos _x, _y, determinan la posición de los rectángulos que funcionan como representación de los ojos
 });
 
-function actualizarDimensiones() {
-  const dimensiones = contenedor.getBoundingClientRect();
+function actualizarDimensiones() {//De declara una función que actualiza las dimensiones del canvas
+  const dimensiones = contenedor.getBoundingClientRect();//Se declara una variable constante en la que se determinan las dimensiones a partir del método getBoundingClientRect, que permite saber el tamaño del elemento y su posición relativa dentro del div main
 
   x = dimensiones.x | 0;
   y = dimensiones.y | 0;
@@ -29,4 +29,4 @@ function actualizarDimensiones() {
   canvas.height = alto = dimensiones.height | 0;
 }
 
-window.addEventListener('resize', actualizarDimensiones);
+window.addEventListener('resize', actualizarDimensiones);//Cuando se cambia el tamaño de la ventana del navegador se recalculan las dimensiones
